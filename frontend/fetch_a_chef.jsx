dfcd511e69c2e4f759c55e7a4c6c457f4fdbec4e
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-01-21 11:57:49 -0500

    Set up auth basics

--- frontend/fetch_a_chef.jsx
+++ frontend/fetch_a_chef.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
   ReactDOM.render(<h1>Testing</h1>, root);
 });
