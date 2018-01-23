9fd01b5cd2d6161f282894a597e8bcb7aa9a933f
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-01-23 11:20:37 -0500

    Finish basic app setup

--- frontend/fetch_a_chef.jsx
+++ frontend/fetch_a_chef.jsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 
 import configureStore from "./store/store";
 
+import Root from "./components/root.jsx";
+
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Testing</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
