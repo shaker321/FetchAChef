340052a2e4c7f88789f032793e95364baaa1cb9b
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-01-23 17:09:06 -0500

    Set up basic login/signup form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
+
+import SessionFormContainer from "./session_form/session_form_container.jsx";
 
 const App = () => (
   <div>
     <h1>FetchAChef</h1>
+
+    <Switch>
+      <Route path="/login" component={ SessionFormContainer } />
+      <Route path="/signup" component={ SessionFormContainer } />
+    </Switch>
   </div>
 );
 
