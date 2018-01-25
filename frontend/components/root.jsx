11e9087331706bff73a50335c7cca18f13e2ad7a
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-01-24 19:57:14 -0500

    Add basebar to site

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,11 +3,13 @@ import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 
 import App from "./app";
+import BaseBar from "./base_bar/base_bar.jsx"
 
 const Root = ({ store }) => (
   <Provider store={ store }>
     <HashRouter>
       <App/>
+      <BaseBar/>
     </HashRouter>
   </Provider>
 );
