f7df9a14cb188ff2d66fdf4467c8637543adef8b
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-01-25 11:52:26 -0500

    Set up basic nav bar buttons

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -4,11 +4,49 @@ import { Link, withRouter } from "react-router-dom";
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentUser: ""
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.loggedIn) {
+      this.setState({ currentUser: this.props.user.username });
+    }
+  }
+
+  loggedOutButtons() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">FetchAChef</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+          <li><Link to="/login">Log In</Link></li>
+        </ul>
+      </div>
+    );
+  }
+
+  loggedInButtons() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">FetchAChef</Link></li>
+          <li><Link to="/">Cart</Link></li>
+          <li><Link to="/">Welcome `${this.state.currentUser}`</Link></li>
+        </ul>
+      </div>
+    );
   }
 
   render() {
+    let buttons;
+
+    this.state.currentUser ? (buttons = this.loggedInButtons()) : (buttons = this.loggedOutButtons());
+
     return (
-      <h2>Here</h2>
+      buttons
     );
   }
 }
