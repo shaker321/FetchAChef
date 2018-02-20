eccb2656d38812851267d4f854033fa6e1c7f096
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-02-20 14:30:40 -0500

    Fix two components being rendered by one path bug

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Route, Redirect, Link, HashRouter } from "react-router-dom";
+import { Switch, Route, Redirect, Link, HashRouter } from "react-router-dom";
 
 import { AuthRoute } from "../util/route_util.jsx";
 
 import BaseBar from "./base_bar/base_bar.jsx";
 import CartContainer from "./nav_bar/buttons/cart/cart_container.jsx";
 import ChangePasswordFormContainer from "./change_password_form/change_password_form_container.jsx";
+import ChefProfileContainer from "./chef_profile/chef_profile_container.jsx";
 import ChefSignUpFormContainer from "./chef_sign_up_form/chef_sign_up_form_container.jsx";
 import ChefTools from "./chef_tools/chef_tools.jsx";
-import ChefProfileContainer from "./chef_profile/chef_profile_container.jsx";
 import KitchenProfileContainer from "./kitchen_profile/kitchen_profile_container.jsx";
 import KitchenSignUpFormContainer from "./kitchen_sign_up_form/kitchen_sign_up_form_container.jsx";
 import KitchenToolsContainer from "./kitchen_tools/kitchen_tools_container.jsx";
@@ -23,17 +23,19 @@ const App = () => (
       <Route path="/" component={ NavBarContainer }/>
     </header>
 
-    <Route exact path="/" component={ MainPageContainer } />
-    <Route exact path="/api/kitchens" component={ SearchPage } />
-    <Route exact path="/api/kitchens/:kitchenId" component={ KitchenProfileContainer } />
-    <Route exact path="/api/chefs/:chefId" component={ ChefProfileContainer } />
-    <Route path="/api/chefs/:chef_id/chef_tools" component={ ChefTools } />
-    <Route path="/api/kitchens/:kitchen_id/kitchen_tools" component={ KitchenToolsContainer } />
-    <Route path="/api/chefs/post" component={ ChefSignUpFormContainer } />
-    <Route path="/api/kitchens/post" component={ KitchenSignUpFormContainer} />
-    <Route exact path="/api/users/:user_id" component={ ChangePasswordFormContainer } />
-    <Route path="/api/users/:user_id/orders" component={ UserOrdersContainer} />
-    <Route path="/api/users/:user_id/cart" component={ CartContainer } />
+    <Switch>
+      <Route exact path="/" component={ MainPageContainer } />
+      <Route exact path="/api/chefs/post" component={ ChefSignUpFormContainer } />
+      <Route exact path="/api/chefs/:chef_id" component={ ChefProfileContainer } />
+      <Route exact path="/api/chefs/:chef_id/chef_tools" component={ ChefTools } />
+      <Route exact path="/api/kitchens/post" component={ KitchenSignUpFormContainer} />
+      <Route exact path="/api/kitchens" component={ SearchPage } />
+      <Route exact path="/api/kitchens/:kitchen_id" component={ KitchenProfileContainer } />
+      <Route exact path="/api/kitchens/:kitchen_id/kitchen_tools" component={ KitchenToolsContainer } />
+      <Route exact path="/api/users/:user_id" component={ ChangePasswordFormContainer } />
+      <Route exact path="/api/users/:user_id/orders" component={ UserOrdersContainer} />
+      <Route exact path="/api/users/:user_id/cart" component={ CartContainer } />
+    </Switch>
 
     <footer>
       <BaseBar/>
