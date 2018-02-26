adc93531caa6afdeb91072167c5cb4c081fe360f
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-02-26 02:09:36 -0500

    Update css

--- frontend/components/kitchen_profile/menu/menu_index_item.jsx
+++ frontend/components/kitchen_profile/menu/menu_index_item.jsx
@@ -11,10 +11,10 @@ class MenuIndexItem extends React.Component {
       <ul className="menu-index-item-content">
         <li className="menu-index-item-title">{ this.props.title }</li>
         <li className="menu-index-item-description">{ this.props.description }</li>
-        <li className="menu-index-item-title">
+        <li className="menu-index-item-description">
           <Link to={ "/api/chefs/" + this.props.chef.id } className="link">{ `Chef ${this.props.chef.first_name} ${this.props.chef.last_name}` }</Link>
         </li>
-        <li className="menu-index-item-title">${ this.props.price } Per Serving</li>
+        <li className="menu-index-item-description">${ this.props.price } Per Serving</li>
       </ul>
     );
   }
