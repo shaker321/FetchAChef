5939218d20264b966686c7296cf9af72ce14085d
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-02-26 01:43:40 -0500

    Restyle kitchen page

--- frontend/components/kitchen_profile/reviews/review_index_item.jsx
+++ frontend/components/kitchen_profile/reviews/review_index_item.jsx
@@ -15,13 +15,23 @@ class ReviewIndexItem extends React.Component {
     ]);
   }
 
+  makeDateHumanReadable(date) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = date.slice(0, 4);
+    let month = months[parseInt(date.slice(5, 7)) - 1];
+    let day = date.slice(8, 10);
+
+    return `${month} ${day}, ${year}`;
+  }
+
   render() {
     return (
       <ul className="review-index-item">
         { this.ratings()[this.props.rating - 1] }
         <li className="review-index-item-body">{ this.props.body }</li>
         <li className="review-index-item-username">{ this.props.username }</li>
-        <li className="review-index-item-username">{ this.props.createdAt }</li>
+        <li className="review-index-item-username">{ this.makeDateHumanReadable(this.props.createdAt) }</li>
       </ul>
     );
   }
