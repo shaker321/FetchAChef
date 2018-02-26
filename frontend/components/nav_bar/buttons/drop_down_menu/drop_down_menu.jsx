7d2b9191f656b6de67da38e9d8a3ad76067777dc
Tony Shaker <anthonypaul.shaker@gmail.com>
2018-02-26 10:52:01 -0500

    Make code DRY and fix relevant CSS

--- frontend/components/nav_bar/buttons/drop_down_menu/drop_down_menu.jsx
+++ frontend/components/nav_bar/buttons/drop_down_menu/drop_down_menu.jsx
@@ -6,85 +6,60 @@ class DropDownMenu extends React.Component {
     super(props);
   }
 
-  redirectToChangePasswordForm(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.props.history.push("/api/users/" + this.props.currentUser.id);
-  }
-
-  redirectToUserOrders(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.props.history.push("/api/users/" + this.props.currentUser.id + "/orders");
-  }
-
-  redirectToChefTools(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.props.history.push("/api/chefs/" + this.props.currentUser.chef.id + "/chef_tools");
-  }
-
-  redirectToKitchenTools(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.props.history.push("/api/kitchens/" + this.props.currentUser.kitchen.id + "/kitchen_tools");
-  }
-
   render() {
     if (this.props.currentUser) {
       let dropDownMenuItems = [
         <li key="change-password">
-          <div
-            onClick={ this.redirectToChangePasswordForm.bind(this) }
+          <Link to={ "/api/users/" + this.props.currentUser.id }
             className="drop-down-menu-item"
             >Change Password
-          </div>
+          </Link>
         </li>,
         <li key="user-orders">
-          <div
-            onClick={ this.redirectToUserOrders.bind(this) }
+          <Link
+            to={ "/api/users/" + this.props.currentUser.id + "/orders" }
             className="drop-down-menu-item"
             >User Orders
-          </div>
+          </Link>
         </li>
       ];
 
       if (this.props.currentUser.kitchen && this.props.currentUser.chef) {
         dropDownMenuItems.push(
           <li key="chef-tools">
-            <div
-              onClick={ this.redirectToChefTools.bind(this) }
+            <Link
+              to={ "/api/chefs/" + this.props.currentUser.chef.id + "/chef_tools" }
               className="drop-down-menu-item"
               >Chef Tools
-            </div>
+            </Link>
           </li>,
           <li key="kitchen-tools">
-            <div
-              onClick={ this.redirectToKitchenTools.bind(this) }
+            <Link
+              to={ "/api/kitchens/" + this.props.currentUser.kitchen.id + "/kitchen_tools" }
               className="drop-down-menu-item"
               >Kitchen Tools
-            </div>
+            </Link>
           </li>
         );
       } else if (this.props.currentUser.kitchen) {
         dropDownMenuItems.push(
           <li key="kitchen-tools">
-            <div
-              onClick={ this.redirectToKitchenTools.bind(this) }
+            <Link
+              to={ "/api/kitchens/" + this.props.currentUser.kitchen.id + "/kitchen_tools" }
               className="drop-down-menu-item"
               >Kitchen Tools
-            </div>
+            </Link>
           </li>
         );
       } else if (this.props.currentUser.chef) {
         dropDownMenuItems.push(
           <li key="chef-tools">
-            <div
-              onClick={ this.redirectToChefTools.bind(this) }
+            <Link
+              to={ "/api/chefs/" + this.props.currentUser.chef.id + "/chef_tools" }
               className="drop-down-menu-item"
               >Chef Tools
-          </div>
-        </li>
+            </Link>
+          </li>
         );
       }
 
